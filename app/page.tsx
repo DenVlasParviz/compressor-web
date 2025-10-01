6672f7b5c588ef30468be56a5a8fc4dcfaa79970
DenVlasParviz <vlas8366@gmail.com>
2025-10-01 14:02:49 +0300

    Code refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,15 @@
 import Image from "next/image";
-import {FileUploader} from "@/components/fileUploader";
+import { FileUploader } from "@/components/fileUploader";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-[#202225] via-[#2f3136] to-[#36393f] relative overflow-hidden">
-      <header className="text-center mb-16">
-        <div className="inline-flex items-center justify-center w-16 h-16 bg-gradient-to-br from-[#5865f2] to-[#7289da] rounded-2xl mb-6 shadow-2xl">
-          <svg
-            className="w-8 h-8 text-white"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"
-            />
-          </svg>
-        </div>
+    <div className="min-h-screen bg-gradient-to-br from-[#202225] via-[#2f3136] to-[#334534] relative overflow-hidden">
+      <header className="text-center mb-16 mt-15">
         <h1 className="text-5xl font-bold text-white mb-2 tracking-tight">
           Video Compressor
         </h1>
         <p className="text-[#b9bbbe] text-lg font-medium">
-          Сжимайте видео быстро и качественно
+          Compress video for discord and get a link
         </p>
       </header>
 
--- components/fileUploader.tsx
+++ components/fileUploader.tsx
@@ -1,43 +1,49 @@
 "use client";
 import { ChangeEvent, useState } from "react";
 import axios from "axios";
+import { CopyButton } from "@/components/ui/shadcn-io/copy-button/index";
 
 export const FileUploader = () => {
   const [file, setFile] = useState<File | null>(null);
-
+  const [link, setLink] = useState("");
   type uploadStatus = "idle" | "uploading" | "success" | "error";
-  const [status,setStatus] = useState<uploadStatus>('idle');
-  const [uploadProgress,setUploadProgress] = useState(0);
+  const [status, setStatus] = useState<uploadStatus>("idle");
+  const [uploadProgress, setUploadProgress] = useState(0);
 
   function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
     if (e.target.files) {
       setFile(e.target.files[0]);
     }
   }
-  async function handleFileUpload() {
-      if (!file) return;
-      setStatus('uploading');
-      setUploadProgress(0);
-      const formData = new FormData();
-      formData.append('file', file);
-      const targetSize = '9';
-      try{
-          await axios.post(`http://localhost:3000/compressor?targetSize=${targetSize}`, formData,{
 
-              onUploadProgress:(progressEvent)=>{
-                 const progress = progressEvent.total ?
-                     Math.round((progressEvent.loaded*100)/progressEvent.total)
-                     : 0;
-                 setUploadProgress(progress)
-              }
-          });
-          setStatus('success');
-          setUploadProgress(100);
-      }catch(e){
-          setStatus('error');
-          setUploadProgress(0);
-          console.log("error uploading:",e)
-      }
+  async function handleFileUpload() {
+    if (!file) return;
+    setStatus("uploading");
+    setUploadProgress(0);
+    const formData = new FormData();
+    formData.append("file", file);
+    const targetSize = "9";
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/compressor?targetSize=${targetSize}`,
+        formData,
+        {
+          onUploadProgress: (progressEvent) => {
+            const progress = progressEvent.total
+              ? Math.round((progressEvent.loaded * 100) / progressEvent.total)
+              : 0;
+            setUploadProgress(progress);
+          },
+        },
+      );
+      setStatus("success");
+      setLink(response.data.videoLink);
+      setUploadProgress(100);
+    } catch (e) {
+      setStatus("error");
+      setUploadProgress(0);
+      console.log("error uploading:", e);
+    }
   }
 
   return (
@@ -57,13 +63,10 @@ export const FileUploader = () => {
             {!file && (
               <>
                 <p className="text-xl font-semibold mb-2 text-white group-hover:text-[#5865f2] transition-colors duration-300">
-                  Перетащи файл сюда
+                  Drag and drop a video file here
                 </p>
                 <p className="text-[#b9bbbe] text-sm font-medium">
-                  или нажми для выбора
-                </p>
-                <p className="text-[#72767d] text-xs mt-2">
-                  MP4, AVI, MOV, MKV • до 2GB
+                  or click to select
                 </p>
               </>
             )}
@@ -82,55 +85,69 @@ export const FileUploader = () => {
       </label>
 
       {/* Compress Button */}
-        {file && status !== "uploading" && (
-            <button
-                onClick={handleFileUpload}
-                className="px-12 py-4 bg-[#5865F2] hover:bg-[#4752C4] text-white font-semibold text-lg transform transition-all rounded-xl duration-300 hover:shadow-xl overflow-hidden hover:scale-105"
+      {file && status !== "uploading" && (
+        <button
+          onClick={handleFileUpload}
+          className="px-12 py-4 bg-[#5865F2] hover:bg-[#4752C4] text-white font-semibold text-lg transform transition-all rounded-xl duration-300 hover:shadow-xl overflow-hidden hover:scale-105"
+        >
+          <div className="relative flex items-center justify-center gap-3">
+            <svg
+              className="w-5 h-5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
             >
-                <div className="relative flex items-center justify-center gap-3">
-                    <svg
-                        className="w-5 h-5"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"
-                        />
-                    </svg>
-                    <span>Compress</span>
-                </div>
-            </button>
-
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"
+              />
+            </svg>
+            <span>Compress</span>
+          </div>
+        </button>
+      )}
+      {status === "uploading" && (
+        <div className="space-y-2">
+          <div className="h-2.5 w-full rounded-full bg-gray-200">
+            <div
+              className="h-2.5 rounded-full bg-blue-600 transition-all duration-300"
+              style={{ width: `${uploadProgress}%` }}
+            ></div>
+            <p className="text-sm text-gray-600">{uploadProgress}% uploaded</p>
+          </div>
+        </div>
+      )}
+      <div>
+        {status === "success" && (
+          <p className="text-[#b9bbbe] text-sm font-medium">success</p>
         )}
-        {status === 'uploading'&&(
-            <div className='space-y-2'>
-<div className='h-2.5 w-full rounded-full bg-gray-200'>
- <div className='h-2.5 rounded-full bg-blue-600 transition-all duration-300' style={{width:`${uploadProgress}%`}}>
-
- </div>
-    <p className='text-sm text-gray-600'>
-        {uploadProgress}% uploaded
-    </p>
-</div>
+        {status === "error" && (
+          <p className="text-[#b9bbbe] text-sm font-medium">error</p>
+        )}
+      </div>
+      <div>
+        {link && (
+          <div className="flex flex-col items-center ">
+            <div className="mt-4 flex flex-colgap-2 ">
+              <CopyButton
+                className="mb-2"
+                content={link}
+                onCopy={() => console.log("Link copied!")}
+              />
+              <span className="px-3 py-1 bg-blue-600  text-gray-200 rounded-xl">
+                {link.slice(0, 40)}...
+              </span>
             </div>
+            <video
+              src={link}
+              controls
+              className="mt-4 rounded-xl shadow-lg w-[320px]"
+            />
+          </div>
         )}
-<div>
-    {status === "success" && (
-        <p className="text-[#b9bbbe] text-sm font-medium">
-            success
-        </p>
-    )}
-    {status === "error" && (
-        <p className="text-[#b9bbbe] text-sm font-medium">
-            error
-        </p>
-    )}
-</div>
+      </div>
     </div>
   );
 };
-
